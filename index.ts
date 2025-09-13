cd461e1d23b487aa048ef05ccd22ab9a0cd50707
sahil-raj <sahilrajaaru@gmail.com>
2025-09-13 18:10:09 +0530

    feat: add bencode dependency and implement peer connection class

--- index.ts
+++ index.ts
@@ -1,1 +1,20 @@
-console.log("Hello via Bun!");
+"use strict";
+
+import fs from "fs";
+import bencode from "bencode";
+
+class connectToPeer {
+  constructor(private file: string) {
+    this.file = file;
+  }
+
+  fetchAnnounceUrl() {
+    const torrentFile = fs.readFileSync(this.file);
+    const parsedFile = bencode.decode(torrentFile);
+
+    if (parsedFile.announce) {
+      const announceUrl = Buffer.from(parsedFile.announce).toString("utf8");
+      console.log("Main announce:", announceUrl);
+    }
+  }
+}
